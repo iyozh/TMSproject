f4e9682103804fdd8789f90e313614d15b4194a9
iyozh <evzhenko1106@gmail.com>
2020-06-12 15:14:44 +0300

    goodbye

--- src/srv.py
+++ src/srv.py
@@ -5,6 +5,7 @@ from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
 
 year = datetime.now().year
+hour = datetime.now().hour
 
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT = {PORT}")
@@ -13,44 +14,56 @@ print(f"PORT = {PORT}")
 class MyHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.startswith("/hello"):
-            return self.hello_start_response()
+        if self.path.startswith("/goodbye"):
+            return self.goodbye_start_response()
         else:
             return SimpleHTTPRequestHandler.do_GET(self)
 
-    def hello_start_response(self):
-        if self.path.startswith("/hello?") or self.path.startswith("/hello/?"):
+    def goodbye_start_response(self):
+        if self.path.startswith("/goodbye?") or self.path.startswith("/goodbye/?"):
             arguments = self.parse_function()
             path = self.path_calculating()
             name = arguments.get("name", "anonymous")
             age = int(arguments.get("age", "0"))
             msg = f"""
-                                   Hello {name}!                
+                                    Hello {name}!                
 
-                                   Your path: {path}
-            
-                                          """
-            if 'age':
-                born = year - age
-                msg += f"\n\t\t\t\t   You were born in {born}"
-            self.response(msg)
+                                    Your path: {path}
+
+                                                       """
+            self.msg_processing(msg, name, path, age)
         else:
             return self.lack_of_qs()
 
+    def msg_processing(self, msg, name, path, age):
+
+        if 'age':
+            born = year - age
+            msg += f"\n\t\t\t\t   You were born in {born}"
+        if hour in range(6, 11):
+            msg += f"\n\t\t\t\t   Good morning!"
+        elif hour in range(12, 24):
+            msg += f"\n\t\t\t\t   Good day!"
+        else:
+            msg += f"\n\t\t\t\t   Good night!"
+
+        self.response(msg)
+
+
     def path_calculating(self):
         if self.path.startswith("/hello/"):
-            path = "/hello/"
+            path = "/goodbye/"
         else:
-            path = "/hello"
+            path = "/goodbye"
 
         return path
 
     def lack_of_qs(self):
         path = self.path_calculating()
         msg = f"""
-                                                     Hello anonymous!                
+                                   Hello anonymous!                
 
-                                                     Your path: {path}
+                                   Your path: {path}
 
                                                             """
         self.response(msg)
