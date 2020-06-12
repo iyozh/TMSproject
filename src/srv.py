591eafe3f72c5529ad24eddb7db5805b57921363
iyozh <evzhenko1106@gmail.com>
2020-06-12 11:37:01 +0300

    editted code

--- src/srv.py
+++ src/srv.py
@@ -9,56 +9,72 @@ year = datetime.now().year
 PORT = int(os.getenv("PORT", 8000))
 print(f"PORT = {PORT}")
 
+
 class MyHandler(SimpleHTTPRequestHandler):
+
     def do_GET(self):
-        if self.path.startswith("/hello/?"):
-            path, qs = self.path.split("?")
-            qs = parse_qs(qs)
-            if 'name' not in qs:
-                name = 'anonymous'
-            else:
-                name = qs['name'][0]
-            if 'age' in qs:
-                age = qs['age'][0]
-                born = year - int(age)
-                msg = f"""
-                        Hello {name}!
-                                        
-                        You were born in {born} year                    
-                                            
-                        Your path: {path}
-                               """
-                self.send_response(200)
-                self.send_header("Content-type", "text/plain")
-                self.send_header("Content-length", len(msg))
-                self.end_headers()
-
-                self.wfile.write(msg.encode())
-            msg = f"""
-                        Hello {name}!
-    
-                        Your path: {path}
-                """
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-length", len(msg))
-            self.end_headers()
-
-            self.wfile.write(msg.encode())
-        elif self.path.startswith("/hello"):
-            msg = f"""
-                        Hello anonymous!
-
-                        Your path: {self.path}
-                            """
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.send_header("Content-length", len(msg))
-            self.end_headers()
-
-            self.wfile.write(msg.encode())
+        if self.path.startswith("/hello"):
+            return self.hello_start_response()
         else:
             return SimpleHTTPRequestHandler.do_GET(self)
+
+    def hello_start_response(self):
+        if self.path.startswith("/hello?") or self.path.startswith("/hello/?"):
+            arguments = self.parse_function()
+            path = self.path_calculating()
+            name = arguments.get("name", "anonymous")
+            age = int(arguments.get("age", "0"))
+            msg = f"""
+                                   Hello {name}!                
+
+                                   Your path: {path}
+            
+                                          """
+            if 'age':
+                born = year - age
+                msg += f"\n\t\t\t\t   You were born in {born}"
+            self.response(msg)
+        else:
+            return self.lack_of_qs()
+
+    def path_calculating(self):
+        if self.path.startswith("/hello/"):
+            path = "/hello/"
+        else:
+            path = "/hello"
+
+        return path
+
+    def lack_of_qs(self):
+        path = self.path_calculating()
+        msg = f"""
+                                                     Hello anonymous!                
+
+                                                     Your path: {path}
+
+                                                            """
+        self.response(msg)
+
+    def parse_function(self):
+        path, qs = self.path.split("?")
+        arguments = {}
+        qs = parse_qs(qs)
+        if 'name' in qs:
+            name = qs['name'][0]
+        for keys, values in qs.items():
+            if not values:
+                continue
+            arguments[keys] = values[0]
+
+        return arguments
+
+    def response(self, msg):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.send_header("Content-length", len(msg))
+        self.end_headers()
+
+        self.wfile.write(msg.encode())
 
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
