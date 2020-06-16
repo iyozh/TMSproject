6f43f8760a60e19364e4d8c5d72f4f0c70c5ae8c
iyozh <evzhenko1106@gmail.com>
2020-06-16 09:14:21 +0300

    good web server

--- src/srv.py
+++ src/srv.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 from http.server import SimpleHTTPRequestHandler
 from urllib.parse import parse_qs
+from typing import Dict
 
 year = datetime.now().year
 hour = datetime.now().hour
@@ -14,66 +15,60 @@ print(f"PORT = {PORT}")
 class MyHandler(SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        if self.path.startswith("/goodbye"):
-            return self.goodbye_start_response()
-        else:
-            return SimpleHTTPRequestHandler.do_GET(self)
+        path = self.path_calculating()
+        handlers = {
+            "/hello": self.hello_response,
+            "/goodbye": self.goodbye_response
+        }
+        handler = handlers.get(path, super().do_GET)
+        handler()
 
-    def goodbye_start_response(self):
-        if self.path.startswith("/goodbye?") or self.path.startswith("/goodbye/?"):
-            arguments = self.parse_function()
-            path = self.path_calculating()
-            name = arguments.get("name", "anonymous")
-            age = int(arguments.get("age", "0"))
-            msg = f"""
+    def hello_response(self):
+        arguments = self.parse_function()
+        path = self.path_calculating()
+        name = self.name_calculating(arguments)
+        age = self.age_calculating(arguments)
+        msg = f"""
                                     Hello {name}!                
 
                                     Your path: {path}
 
                                                        """
-            self.msg_processing(msg, name, path, age)
-        else:
-            return self.lack_of_qs()
-
-    def msg_processing(self, msg, name, path, age):
-
         if 'age':
             born = year - age
             msg += f"\n\t\t\t\t   You were born in {born}"
-        if hour in range(6, 11):
-            msg += f"\n\t\t\t\t   Good morning!"
-        elif hour in range(12, 24):
-            msg += f"\n\t\t\t\t   Good day!"
-        else:
-            msg += f"\n\t\t\t\t   Good night!"
 
         self.response(msg)
 
+    def goodbye_response(self):
+        if hour in range(6, 11):
+            msg = f"\n\t\t\t\t   Good morning!"
+        elif hour in range(12, 24):
+            msg = f"\n\t\t\t\t   Good day!"
+        else:
+            msg = f"\n\t\t\t\t   Good night!"
+
+        self.response(msg)
 
     def path_calculating(self):
-        if self.path.startswith("/hello/"):
-            path = "/goodbye/"
-        else:
-            path = "/goodbye"
+        return self.path.split("?")[0]
 
-        return path
+    def name_calculating(self, qs_arguments: Dict):
+        return qs_arguments.get('name', 'anonymous')
 
-    def lack_of_qs(self):
-        path = self.path_calculating()
-        msg = f"""
-                                   Hello anonymous!                
-
-                                   Your path: {path}
-
-                                                            """
-        self.response(msg)
+    def age_calculating(self, qs_arguments: Dict):
+        return int(qs_arguments.get('age', 0))
 
     def parse_function(self):
-        path, qs = self.path.split("?")
+        _path, *qs = self.path.split("?")
         arguments = {}
+
+        if len(qs) != 1:
+            return arguments
+
+        qs = qs[0]
         qs = parse_qs(qs)
-        if 'name' in qs:
-            name = qs['name'][0]
+
         for keys, values in qs.items():
             if not values:
                 continue
