624bd1e2a1103a6c86687b425e44d43839dcac1e
iyozh <evzhenko1106@gmail.com>
2020-07-10 21:23:36 +0300

    files 5

--- src/pages/goodbye.py
+++ src/pages/goodbye.py
@@ -6,7 +6,7 @@ from src.pages.stats import visits_counter
 hour = datetime.datetime.now().hour
 
 
-def get_page_goodbye(self, method, path):
+def get_page_goodbye(server, method, path):
     visits_counter(path)
     if hour in range(6, 11):
         msg = f"\n\t\t\t\t   Good morning!"
@@ -15,4 +15,4 @@ def get_page_goodbye(self, method, path):
     else:
         msg = f"\n\t\t\t\t   Good night!"
 
-    respond_200(self, msg, "text/plain")
+    respond_200(server, msg, "text/plain")
