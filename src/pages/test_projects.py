67dcf44a717fe77025b47ad7b476375d677faf01
iyozh <evzhenko1106@gmail.com>
2020-07-14 09:50:50 +0300

    projects editing on django

---
+++ src/pages/test_projects.py
@@ -0,0 +1,126 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_http_methods
+
+from errors import Missing_Data
+from utils.file_utils import get_content
+from utils.json_utils import get_json, save_data
+from path import PORTFOLIO, PROJECTS, PROJECTS_INDEX, PROJECTS_TEMPLATE
+from utils.session_utils import parse_user_sessions
+from src.pages.stats import visits_counter
+
+@require_http_methods(['GET', 'POST'])
+def projects_handler(request):
+    switcher = {
+        "GET": projects_GETresponse,
+        "POST": projects_editing_handler,
+    }
+    handler = switcher[request.method]
+
+    try:
+        return handler(request,"/test_projects")
+    except Missing_Data:
+        return HttpResponse("You miss something...")
+
+
+def projects_editing_handler(request, redirect):
+    switcher = {
+        "/test_projects/editing/add": add_project,
+        "/test_projects/editing/delete": delete_project,
+        "/test_projects/editing/change": change_project,
+    }
+
+    handler = switcher[request.path]
+    return handler(request, redirect)
+
+
+def projects_GETresponse(request,redirect):
+    visits_counter(request.path)
+    projects_content = get_json(PROJECTS)
+    template = get_content(PROJECTS_TEMPLATE)
+    projects = ""
+
+    for project in projects_content:
+        projects += template.format(
+            project_name=projects_content[project]["project_name"],
+            project_id=project,
+            project_date=projects_content[project]["project_date"],
+            project_description=projects_content[project]["project_description"],
+        )
+
+    page_content = get_content(PROJECTS_INDEX).format(projects=projects)
+    return HttpResponse(page_content, "text/html")
+
+
+def get_editing_page(request):
+    visits_counter(request.path)
+    edit_page = get_content(PORTFOLIO / "test_projects" / "edit_projects.html")
+    return HttpResponse(edit_page, "text/html")
+
+
+def add_project(request,redirect):
+    form_content = parse_user_sessions(request)
+
+    if (
+        "project_name" not in form_content
+        or "project_id" not in form_content
+        or "project_description" not in form_content
+        or "project_date" not in form_content
+    ):
+        raise Missing_Data()
+
+    projects = get_json(PROJECTS)
+
+    new_project = {}
+
+    id_new_project = form_content["project_id"]
+
+    if id_new_project in projects:
+        raise Missing_Data()
+
+    new_project[id_new_project] = {
+        "project_name": "",
+        "project_description": "",
+        "project_date": "",
+    }
+
+    for item in form_content:
+        if item in new_project[id_new_project]:
+            new_project[id_new_project][item] = form_content[item]
+
+    projects.update(new_project)
+
+    save_data(PROJECTS, projects)
+
+    return HttpResponseRedirect(redirect)
+
+
+def delete_project(request,redirect):
+    form = parse_user_sessions(request)
+    projects = get_json(PROJECTS)
+
+    if "project_id" not in form:
+        raise Missing_Data()
+
+    project_id = form["project_id"]
+
+    if project_id not in projects:
+        raise Missing_Data()
+
+    projects.pop(project_id)
+    save_data(PROJECTS, projects)
+    return HttpResponseRedirect(redirect)
+
+
+def change_project(request,redirect):
+    form = parse_user_sessions(request)
+    projects = get_json(PROJECTS)
+
+    if "project_id" not in form:
+        raise Missing_Data()
+
+    for item in form:
+        if item != "project_id":
+            projects[form["project_id"]][item] = form[item]
+
+    save_data(PROJECTS, projects)
+    return HttpResponseRedirect(redirect)
--- src/project/urls.py
+++ src/project/urls.py
@@ -25,6 +25,7 @@ from pages.hello import handler_hello
 from pages.projects import get_projects_page, get_projects_css
 from pages.resume import get_portfolio, get_resume_css
 from pages.stats import get_stats
+from pages.test_projects import projects_handler, get_editing_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,5 +37,11 @@ urlpatterns = [
     path('hello/', handler_hello),
     path('stats/', get_stats),
     path('education/', edu_handler),
-    path('education/night_mode', edu_handler)
+    path('education/night_mode', edu_handler),
+    path('test_projects/', projects_handler),
+    path('test_projects/editing', get_editing_page),
+    path('test_projects/editing/add', projects_handler),
+    path('test_projects/editing/change', projects_handler),
+    path('test_projects/editing/delete', projects_handler),
+
 ]
