618a5cb8ebdf87788c438cd37c3ae09878faf284
iyozh <evzhenko1106@gmail.com>
2020-08-07 19:25:07 +0300

    some migrations

--- src/applications/stats/models.py
+++ src/applications/stats/models.py
@@ -7,7 +7,7 @@ class Stats(models.Model):
     url = models.URLField(null=True, blank=True)
     date = models.DateTimeField(null=True, blank=True)
     method = models.TextField(null=True, blank=True)
-    user = models.TextField(blank=True, null=True)
+    user = models.TextField(null=True,blank=True)
 
     class Meta:
         ordering = ["pk"]
